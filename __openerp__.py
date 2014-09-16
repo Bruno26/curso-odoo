2a4938a4254a7dcd14ba8e205d3c04f4368b8eef
Bruno Palacios <usuario@localhost.localdomain>
2014-09-16 11:06:34 -0430

    Agregando archivo para declarar modulos

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#Diccionario necesario para crear u n moduloe en 0penERP
+
+{
+	'name': 'Curso OpenEPR',
+	'description': 'Este módulo es para aprender en OpenEPR',
+	'author':'Bruuno Palacios',
+	'version':'dia1',
+	'depends': ['base', ],
+	'data': [],
+	'demo': [],
+}
