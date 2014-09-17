97568ee4a0584065dd2a7fd4aa4d64c6fc76e8ba
Bruno Palacios <bpalacios@minmujer.gob.ve>
2014-09-17 18:01:56 -0430

    Agregando vistas, modelos e instanciando en openerp.py agregando secuencia generadora de codigo en suscripcion

--- models/solicitud.py
+++ models/solicitud.py
@@ -6,13 +6,15 @@ from openerp.osv import osv, fields
 class solicitud(osv.osv):
 	_name = 'co.solicitud'
 	_description ='CO Solicitud'
+	_rec_name = 'code'
 
 	_columns={
+		'code':fields.char('Código'),
 		'suscriptor_id':fields.many2one('co.suscriptor','Afiliado'),
 		'multimedia_id':fields.many2one('co.multimedia','Multimedia'),
 		'medio_id':fields.many2one('co.tipo.medio','Tipo de Medio'),
 		'tienda_id':fields.many2one('co.tienda','Origen'),
-		'requested_date':fields.date('Fecha Solicitud'),
+		'requested_date':fields.date('Fecha solicitud'),
 		'qty_days':fields.integer('Duración (en días)'),
 	}
 	
